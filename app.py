3323beac0f55532944ce1aaba65a27c3b1ab4e91
#.insert3434.# <naumanrafique10@gmail.com>
2025-03-18 00:17:17 +0500

    Confusion Metrics added

--- app.py
+++ app.py
@@ -68,4 +68,9 @@ y_pred = pipeline.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 
-accuracy
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
+
+cm = confusion_matrix(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+
